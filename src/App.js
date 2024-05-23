2b420468a8dfca1317d3a6bc3a161b970372e883
SavaCatalin1 <savactln0@gmail.com>
2024-05-23 12:35:29 +0300

    vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import './App.css';
 import Home from "./Home/Home";
 import Navbar from "./Navbar/Navbar";
@@ -43,6 +44,7 @@ function App() {
           </div>
         </div>
       </BrowserRouter>
+      <Analytics />
     </div>
   );
 }
